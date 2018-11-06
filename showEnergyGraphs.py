ba336a9d1a6d7394225f532699896fb5cee8bd27
Mauricio Imaeda <>
2018-11-06 19:35:43 -0200

    [FIX] Adicionado CSV com dados cropados

--- showEnergyGraphs.py
+++ showEnergyGraphs.py
@@ -79,7 +79,8 @@ for actor_features in feature_data_frames:
             createFolder('./dataCropped/Actor_{0:0>2}/Frase_{1}/'.format(
                 i, j))
             array = numpy.transpose(array)
-            numpy.savetxt(path, array, fmt='%f', delimiter=',', header="Zero Crossing Rate, Energy, Entropy of Energy, Spectral Centroid, Spectral Spread, Spectral Entropy, Spectral Flux, Spectral Rolloff, MFCC1, MFCC2, MFCC3, MFCC4, MFCC5, MFCC6, MFCC7, MFCC8, MFCC9, MFCC10, MFCC11, MFCC12, MFCC13, Chroma Vector 1, Chroma Vector 2, Chroma Vector 3, Chroma Vector 4, Chroma Vector 5, Chroma Vector 6, Chroma Vector 7, Chroma Vector 8, Chroma Vector 9, Chroma Vector 10, Chroma Vector 11, Chroma Vector 12, Chroma Deviation")
+            numpy.savetxt(path, array, fmt='%f', delimiter=',')
+            # numpy.savetxt(path, array, fmt='%f', delimiter=',', header="Zero Crossing Rate, Energy, Entropy of Energy, Spectral Centroid, Spectral Spread, Spectral Entropy, Spectral Flux, Spectral Rolloff, MFCC1, MFCC2, MFCC3, MFCC4, MFCC5, MFCC6, MFCC7, MFCC8, MFCC9, MFCC10, MFCC11, MFCC12, MFCC13, Chroma Vector 1, Chroma Vector 2, Chroma Vector 3, Chroma Vector 4, Chroma Vector 5, Chroma Vector 6, Chroma Vector 7, Chroma Vector 8, Chroma Vector 9, Chroma Vector 10, Chroma Vector 11, Chroma Vector 12, Chroma Deviation")
             array = []
         k =0
     j=0    
