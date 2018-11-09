5a6fafc0e22fbd47b3dc4b2afbaa0785fcea9830
Bruno Andreetto <bruno.andreetto@usp.br>
2018-11-09 14:27:40 -0200

    Fix graph quantity log

--- functions.py
+++ functions.py
@@ -171,7 +171,6 @@ def showGraphs(domain, **kwargs):
     colors = ['r', 'g', 'b', 'y']
     i = 0
     for actor_features in features_data_frames:
-        print '{} graphs loaded'.format(i)
         for phrase_features in actor_features:
             for emotion_features in phrase_features:
                 for feature in features_range:
@@ -179,5 +178,6 @@ def showGraphs(domain, **kwargs):
                     i += 1
                     plt.plot(
                         range(0, len(emotion_features[feature])), emotion_features[feature], color)
+        print '{} graphs loaded'.format(i)
 
     plt.show()
