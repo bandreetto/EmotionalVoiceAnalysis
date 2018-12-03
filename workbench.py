996280df7a54d6e02d4e94e3b1a7ddf5e1cf37f2
Bruno Andreetto <bruno.andreetto@usp.br>
2018-12-03 02:09:07 -0200

    Extract variables to top of file

--- workbench.py
+++ workbench.py
@@ -10,6 +10,10 @@ from sklearn.metrics import accuracy_score
 from utils import *
 from tabulate import tabulate
 
+n_testing_slices = 1
+categories_deviation = .01
+explained_variance = .99
+
 # data_frames = get_features_data_frames('time')
 
 # print get_natural_frequency(data_frames[1][0][0]['Energy'])
@@ -26,11 +30,12 @@ unified_data_frame = unify_data_frames(data_frames, feature_reducers)
 # {
 # 1: 'Happy', 2: 'Sad', 3: 'Angry', 4: 'Fearful'})
 
-pca_data_frame = apply_pca(unified_data_frame, n_components=.99)
+pca_data_frame = apply_pca(unified_data_frame, n_components=explained_variance)
 
 components = list(set(pca_data_frame) - set(['Emotion']))
 for component in components:
-    pca_data_frame[component] = categorize_data(.01, pca_data_frame[component])
+    pca_data_frame[component] = categorize_data(
+        categories_deviation, pca_data_frame[component])
 data = pca_data_frame.loc[:, components].values
 emotions = pca_data_frame.loc[:, ['Emotion']].values
 
@@ -47,12 +52,10 @@ data = shuffle_array(data, positions_to_shuffle)
 # emotions = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
 # data = [["Teste 1"], ["Teste 2"], ["Teste 3"], ["Teste 4"], ["Teste 5"], ["Teste 6"], ["Teste 7"], ["Teste 8"], ["Teste 9"], ["Teste 10"]]
 
-n_slices = 1
+emotions_splitted = split_list(emotions, n_testing_slices)
+data_splitted = split_list(data, n_testing_slices)
 
-emotions_splitted = split_list(emotions, n_slices)
-data_splitted = split_list(data, n_slices)
-
-for iteration in range(0, n_slices):
+for iteration in range(0, n_testing_slices):
     emotion_train, emotion_test = get_train_test_array(
         emotions_splitted, iteration)
     data_train, data_test = get_train_test_array(data_splitted, iteration)
